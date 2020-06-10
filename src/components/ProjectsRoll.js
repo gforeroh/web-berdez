5298e3dabb50bbd36bdc1fdc85a392c738979aeb
Gerardo Forero <gerardo.forero@linktic.com>
2020-06-10 16:05:00 -0500

    Ajustes

---
+++ src/components/ProjectsRoll.js
@@ -0,0 +1,15 @@
+import React from 'react'
+import PropTypes from 'prop-types'
+import { Link, graphql, StaticQuery } from 'gatsby'
+import PreviewCompatibleImage from './PreviewCompatibleImage'
+
+
+class ProjectsRoll extends React.Component {
+    render(){
+        return(
+            <h1>Probando hasta aqui</h1>
+        )
+    }
+}
+ 
+export default ProjectsRoll;
